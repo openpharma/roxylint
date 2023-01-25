e255de973ddfdc9beee8265de8e2669be6d16e99
dgkf <18220321+dgkf@users.noreply.github.com>
2023-01-25 18:06:44 -0500

    updating docs; buildignore

--- R/roxy_alerts.R
+++ R/roxy_alerts.R
@@ -1,6 +1,15 @@
+#' Capture output and re-emit as a cli alert
+#'
+#' @param tag A [roxygen2::roxy_tag()] to use as context for the alert.
+#' @param expr An expression to evaluate.
+#'
+#' @return `TRUE`, invisibly.
+#'
+#' @importFrom utils capture.output
+#' @keywords internal
 into_roxy_alert <- function(tag, expr) {
   res <- withCallingHandlers(
-    capture.output({
+    utils::capture.output({
       eval(expr)
       invisible(NULL)
     }),
@@ -17,8 +26,18 @@ into_roxy_alert <- function(tag, expr) {
   if (length(res) > 0) {
     cli::cli_alert(paste(format_tag_prefix(tag), paste0(res, collapse = " ")))
   }
+
+  invisible(TRUE)
 }
 
+
+#' Format a tag for alert context
+#'
+#' @param x A [roxygen2::roxy_tag()].
+#'
+#' @return A formatted string
+#'
+#' @keywords internal
 format_tag_prefix <- function(x) {
   # inspired largely by roxygen2::link_to and roxygen2::warn_roxy_tag
 
